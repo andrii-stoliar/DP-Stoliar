ee98bfbd2757d709f25ed3d252814495779b8bb2
andrii-stoliar <xstoliar@stuba.sk>
2025-12-01 17:46:54 +0100

    Prepinac - zaciatok

--- ML/TS/PID/PI_simple_forPB/real/mer_sprac/sprac.m
+++ ML/TS/PID/PI_simple_forPB/real/mer_sprac/sprac.m
@@ -21,25 +21,25 @@ T = table(time, spir, snimac1, setpoint1, vent, 'VariableNames', {'time','spir',
 fname = fullfile('ts_pb_vent6_spir4_PI_real.csv');
 writetable(T, fname); 
 
-% clc; clear; 
-% load pb_vent6_spir4_pbTopb.mat
+clc; clear; 
+load pb_vent6_spir4_pbTopb.mat
 
-% time = time(2300:3100) - time(2300);
-% spir = spir(2300:3100) - 4;
-% snimac1 = snimac1(2300:3100) - snimac1(2300);
-% vent = vent(2300:3100);
-% setpoint1 = setpoint1(2300:3100);
-% setpoint1(1:100) = 0;
+time = time(2300:3100) - time(2300);
+spir = spir(2300:3100) - 4;
+snimac1 = snimac1(2300:3100) - snimac1(2300);
+vent = vent(2300:3100);
+setpoint1 = setpoint1(2300:3100);
+setpoint1(1:100) = 0;
 
-% figure(2);
-% plot(time, spir)
-% hold on
-% plot(time, snimac1)
-% hold on
-% plot (time, setpoint1)
-% ylim([-4 6])
-% hold off
+figure(2);
+plot(time, spir)
+hold on
+plot(time, snimac1)
+hold on
+plot (time, setpoint1)
+ylim([-4 6])
+hold off
 
-% T = table(time, spir, snimac1, setpoint1, vent, 'VariableNames', {'time','spir','snimac1','setpoint','vent'});
-% fname = fullfile('ts_pb_vent6_pbTopb_real_badToShow.csv');
-% writetable(T, fname); 
+T = table(time, spir, snimac1, setpoint1, vent, 'VariableNames', {'time','spir','snimac1','setpoint','vent'});
+fname = fullfile('ts_pb_vent6_pbTopb_real_badToShow.csv');
+writetable(T, fname); 
