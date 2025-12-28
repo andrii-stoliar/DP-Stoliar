77898d379019019d3e35724491922e006e932b11
andrii-stoliar <xstoliar@stuba.sk>
2025-12-28 20:43:08 +0100

    1.3 was corrected

--- ML/TS/PID/PI_simple_forPB/real/mer_sprac/sprac.m
+++ ML/TS/PID/PI_simple_forPB/real/mer_sprac/sprac.m
@@ -1,33 +1,35 @@
-% clc; clear; close all;
-% load pb_vent6_spir4_pif4.mat
+clc; clear; close all;
+load pb_vent6_spir4_pif4.mat
 
-% time = time(5999:6500) - time(5999);
-% spir = spir(5999:6500) - 4;
-% snimac1 = snimac1(5999:6500) - snimac1(6000);
-% vent = vent(5999:6500);
-% setpoint1 = setpoint1(5999:6500);
+time = time(5999:6500) - time(5999);
+spir = spir(5999:6500) - 4;
+snimac1 = snimac1(5999:6500) - snimac1(6000);
+vent = vent(5999:6500);
+setpoint1 = setpoint1(5999:6500);
 
-% figure(1);
-% plot(time, spir)
-% hold on
-% plot(time, snimac1)
-% hold on
-% plot (time, setpoint1)
-% ylim([-4 6])
-% hold off
+figure(1);
+plot(time, spir)
+hold on
+plot(time, snimac1)
+hold on
+plot (time, setpoint1)
+ylim([-4 6])
+hold off
 
-% T = table(time, spir, snimac1, setpoint1, vent, 'VariableNames', {'time','spir','snimac1','setpoint','vent'});
-% fname = fullfile('ts_pb_vent6_spir4_PI_real.csv');
-% writetable(T, fname); 
+T = table(time, spir, snimac1, setpoint1, vent, 'VariableNames', {'time','spir','snimac1','setpoint','vent'});
+fname = fullfile('ts_pb_vent6_spir4_PI_real.csv');
+writetable(T, fname); 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clc; clear; 
 load pb_vent6_spir4_pbTopb.mat
 
-time = time(1399:end) - time(1399);
-spir = spir(1399:end);
-snimac1 = snimac1(1399:end);
-vent = vent(1399:end);
-setpoint1 = setpoint1(1399:end);
+time = time(1799:end) - time(1799);
+spir = spir(1799:end);
+snimac1 = snimac1(1799:end);
+vent = vent(1799:end);
+setpoint1 = setpoint1(1799:end);
 setpoint1 = setpoint1;
 
 figure(2);
@@ -38,6 +40,6 @@ hold on
 plot (time, setpoint1)
 hold off
 
-% T = table(time, spir, snimac1, setpoint1, vent, 'VariableNames', {'time','spir','snimac1','setpoint','vent'});
-% fname = fullfile('ts_pb_vent6_pbTopb_real_badToShow.csv');
-% writetable(T, fname); 
+T = table(time, spir, snimac1, setpoint1, vent, 'VariableNames', {'time','spir','snimac1','setpoint','vent'});
+fname = fullfile('ts_pb_vent6_pbTopb_real_badToShow.csv');
+writetable(T, fname); 
